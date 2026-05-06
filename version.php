f36b6d613a569ac83f7400a601b226fe0ae18bc1
ramhee98 <32970397+ramhee98@users.noreply.github.com>
2026-05-06 10:57:42 +0200

    chore: declare plugin maturity and release version

--- version.php
+++ version.php
@@ -27,3 +27,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_fhgr_code_server';
 $plugin->version = 2026050600;
 $plugin->requires = 2025041700; // Moodle 5.0+
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.1.0';
